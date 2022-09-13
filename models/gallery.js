3877ce973a517b1443fb2c19b4688018c4fe39d6
Peter Lam <plam1216@gmail.com>
2022-09-13 14:42:52 -0400

    gallery model with nested schema

--- models/gallery.js
+++ models/gallery.js
@@ -2,17 +2,24 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const imageSchema = new Schema({
-    imageName: String,
-    imageURL: {type: String, required: true},
-    imageDescription: String
-})
+        imageName: String,
+        imageURL: {type: String, required: true},
+        imageDescription: String
+    }, {
+        // timestamps: true -> Mongoose creates createdAt and updatedAt properties with type Date for when the document was created and last updated
+        timestamps: true
+    }
+)
 
 const gallerySchema = new Schema({
     galleryName: String,
     coverImage: String,
     galleryDescription: String,
     galleryImages: [imageSchema]
-})
+    }, {
+    timestamps: true
+    }
+)
 
 const Gallery = mongoose.model('Gallery', gallerySchema)
 
