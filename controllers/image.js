5436220ed2010d391f70d4b5adeacb7e06eebad7
Peter Lam <plam1216@gmail.com>
2022-09-12 23:58:05 -0400

    edit update and delete routes added

--- controllers/image.js
+++ controllers/image.js
@@ -1,21 +1,40 @@
 // DEPENDENCIES
 const express = require('express')
-const { isObjectIdOrHexString } = require('mongoose')
+const mongoose = require('mongoose')
 const imageRouter = express.Router()
 // Image schema is nested inside Gallery schema
 const Gallery = require('../models/gallery.js')
 
 // DELETE
+imageRouter.delete('/gallery/:id/image/:imgId', (req, res) => {
+    galId = mongoose.Types.ObjectId(`${req.params.id}`)
+    imgId = mongoose.Types.ObjectId(`${req.params.imgId}`)
+
+    Gallery.findOneAndUpdate({'galleryImages._id': imgId}, {$pull: {galleryImages:{_id:imgId}}}, () => {
+        res.redirect(`/gallery/${req.params.id}`)
+    })
+})
+
 
 // UPDATE
+imageRouter.put('/gallery/:id/image/:imgId', (req, res) => {
+    galId = mongoose.Types.ObjectId(`${req.params.id}`)
+    imgId = mongoose.Types.ObjectId(`${req.params.imgId}`)
 
+    let updateImg = {$set: {}}
+    for(param in req.body) {
+        updateImg.$set['galleryImages.$.'+param] = req.body[param]
+    }
+
+    Gallery.findOneAndUpdate({'_id': galId, 'galleryImages._id':imgId}, updateImg, () => {
+        res.redirect(`/gallery/${req.params.id}/`)
+    })
+})
 
 // CREATE
 imageRouter.post('/gallery/:id/image', (req, res) => {
     // find the gallery we clicked on
     Gallery.findById(req.params.id, (err, foundGallery) => {
-        // console.log('req.body', req.body)
-        // console.log('Found Gallery', foundGallery)
         // push the req.body into the galleryImages array which has the Image schema
         // this fills the values for the Image schema
         foundGallery.galleryImages.push(req.body);
@@ -30,19 +49,12 @@ imageRouter.post('/gallery/:id/image', (req, res) => {
 // EDIT
 imageRouter.get('/gallery/:id/image/:imgid/edit', (req, res) => {
     Gallery.findById(req.params.id, (err, foundGallery) => {
-        // console.log("GALLERY ID", req.params.id)
-        // console.log("IMAGE ID", req.params.imgid)
-        // console.log('foundGallery id', foundGallery.galleryImages[0]._id)
-        // console.log('[0] to str',foundGallery.galleryImages[0]._id.toString())
-        // console.log('[0] length',foundGallery.galleryImages.length)
         for(let i = 0; i < foundGallery.galleryImages.length; i++) {
-        // console.log('forloop galleryimageslength', foundGallery.galleryImages.length)
-        // console.log('forloop galleryimages', foundGallery.galleryImages[i])
         // .toString() to convert the Mongodb Object ID to a string
             if(foundGallery.galleryImages[i]._id.toString() === req.params.imgid) {
                 res.render(`image/edit.ejs`, {
                     gallery: foundGallery,
-                    galleryImage: foundGallery.galleryImages[i] 
+                    galleryImage: foundGallery.galleryImages[i]
                 })
             }
         }
